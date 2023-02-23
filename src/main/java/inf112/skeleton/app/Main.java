24e833d4de644b2f6c6fa0ee8e41695c047c1851
Asdf1234 <bjornhagen3@live.no>
2023-02-23 18:08:00 +0100

    Commitet til main (gjorde feil) har slettet det

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -4,16 +4,15 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-import inf112.skeleton.app.game.SouthGame;
-
 
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("hello-world");
-        cfg.setWindowedMode(1080, 720);
+        cfg.setWindowedMode(480, 320);
 
-        new Lwjgl3Application(new SouthGame(), cfg);
+        new Lwjgl3Application(new HelloWorld(), cfg);
     }
 
 }
+
--- src/main/java/inf112/skeleton/app/UI/Inventory.java
+++
@@ -1,5 +0,0 @@
-package inf112.skeleton.app.UI;
-
-public class Inventory {
-    
-}
