269c351807153bfd26e0efc3010634be2dd8113c
Asdf1234 <bjornhagen3@live.no>
2023-02-23 18:01:19 +0100

    strukturforslag

---
+++ src/main/java/inf112/skeleton/app/Input/UserInput.java
@@ -0,0 +1,27 @@
+package inf112.skeleton.app.Input;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Gdx;
+
+
+public class UserInput {
+
+    
+
+
+    public static int returnKeyPressed(){
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            return 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            return 2;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            return 3;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            return 4;
+        }
+        else {return 0;}
+    }
+    
+}
--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -4,14 +4,17 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import inf112.skeleton.app.Game.SouthGame;
+
 
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("hello-world");
-        cfg.setWindowedMode(480, 320);
+        cfg.setWindowedMode(1080, 720);
 
-        new Lwjgl3Application(new HelloWorld(), cfg);
+        new Lwjgl3Application(new SouthGame(), cfg);
     }
 
 }
+
