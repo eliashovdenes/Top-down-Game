4792dc3e290e87d0c33b0df807cffe46908e6204
Asdf1234 <bjornhagen3@live.no>
2023-02-23 17:58:35 +0100

    Strukturforslag (riktig branch denne gang..)

---
+++ src/main/java/inf112/skeleton/app/Game/Entities/Entities/Obstacle.java
@@ -0,0 +1,14 @@
+package inf112.skeleton.app.Game.Entities.Entities;
+
+import inf112.skeleton.app.Game.Entities.AbstractEntity;
+
+public class Obstacle extends AbstractEntity{
+    /*
+     * The Obstacle class will extend the Entity class and represent obstacles that the player or other
+     * entities cannot pass through. It will have methods for updating the state of the obstacle and 
+     * handling events that involve the obstacle.
+     * 
+     * 
+   
+     */
+}
