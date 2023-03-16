3bcd6851264107a2c3cb7d2988db04ae6e58a67a
brorby <magnus.sbr12@gmail.com>
2023-03-16 11:29:48 +0100

    hardkodet merge med elias map

--- src/main/java/inf112/skeleton/app/Maps.java
+++ src/main/java/inf112/skeleton/app/Maps.java
@@ -1,9 +1,10 @@
 package inf112.skeleton.app;
 
 public enum Maps {
-    Level1("src/main/java/inf112/skeleton/app/assets/Level 1.tmx"),
-    Level2("src/main/java/inf112/skeleton/app/assets/Level 2.tmx"), //src\main\java\inf112\skeleton\app\assets\Level 2.tmx
-    House("src/main/java/inf112/skeleton/app/assets/House.tmx");
+    Level1("src/main/java/inf112/skeleton/app/assets/maps/Level 1.tmx"),
+    Level2("src/main/java/inf112/skeleton/app/assets/maps/Level 2.tmx"), //src\main\java\inf112\skeleton\app\assets\Level 2.tmx
+    House("src/main/java/inf112/skeleton/app/assets/maps/House.tmx"),
+    Cave("src/main/java/inf112/skeleton/app/assets/maps/Cave.tmx");
     
 
     String source;
