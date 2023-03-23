3df273d7de71b31dfb0660d66cd0c3eec4527b6b
Bjorn <bjornhagen3@live.no>
2023-03-23 11:11:19 +0100

    javadoc Map interface

--- src/main/java/inf112/skeleton/app/Mapfolder/MapInterface.java
+++ src/main/java/inf112/skeleton/app/Mapfolder/MapInterface.java
@@ -5,13 +5,55 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 
 public interface MapInterface {
+    /**
+     * 
+     * @return an integer that represent amount of enemies to be spawned at map
+     */
     public int getEnemies();
+
+    /**
+     * 
+     * @return TiledMap object associated with the map.
+     */
     public TiledMap getMap();
+
+    /**
+     * 
+     * @return OrthogonalTiledMapRenderer object associated with the map.
+     */
     public OrthogonalTiledMapRenderer getRenderer();
+
+    /**
+     * @return  x-coordinate spawn point
+     */
     public float getPlayerSpawnX();
+
+    /**
+     * @return y-coordinate spawn point
+     */
     public float getPlayerSpawnY();
+
+    /**
+     * 
+     * @return from X coordinate bounds for spawn location
+     */
     public int getEnemyBoundsFromX();
+
+    /**
+     * 
+     * @return to X coordinate bounds for spawn location
+     */
     public int getEnemyBoundsToX();
+
+    /**
+     * 
+     * @return from Y coordinate bounds for spawn location
+     */
     public int getEnemyBoundsFromY();
+
+    /**
+     * 
+     * @return to Y coordinate bounds for spawn location.
+     */
     public int getEnemyBoundsToY();    
 }
