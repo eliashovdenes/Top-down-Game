bfc0288820fa35c6d59388b932712982ccf7cbff
HC <hlo028@uib.no>
2023-03-23 12:50:06 +0100

    Implementerte setup(), spawn() og getMonsters()
    Bruker MonsterFactory til å lage monster.
    I setup setter vi opp fabrikkene vi trenger til mapet og velger hvilke
    og hvor mange fiender vi skal lage

--- src/main/java/inf112/skeleton/app/Mapfolder/Cave.java
+++ src/main/java/inf112/skeleton/app/Mapfolder/Cave.java
@@ -1,12 +1,21 @@
 package inf112.skeleton.app.Mapfolder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+import inf112.skeleton.app.Entities.MonsterFactory;
+import inf112.skeleton.app.Entities.MonsterInterface;
+import inf112.skeleton.app.Entities.Enemies.BlueEnemy;
+
 public class Cave extends TiledMap implements MapInterface{
     
-    private int enemies = 2;
+    private ArrayList<String> enemyList;
     private float PlayerSpawnX = 120;
     private float PlayerSpawnY = 77;
     private int EnemyBoundsfromX = 122;
@@ -16,16 +25,35 @@ public class Cave extends TiledMap implements MapInterface{
 
     private TiledMap tiledMap;
     private OrthogonalTiledMapRenderer renderer;
+    private ArrayList<MonsterInterface> monsterList = new ArrayList<>();
+    private Map<String, MonsterFactory> monsterFactories = new HashMap<>();
 
     public Cave(){
         tiledMap = new TmxMapLoader().load(Maps.Cave.source);
         renderer = new OrthogonalTiledMapRenderer(tiledMap);
+        setup();
+        this.spawn(enemyList);
     }
     
+    public void setup() {
+        MonsterFactory blueEnemyFactory = BlueEnemy.getFactory();
+        monsterFactories.put(blueEnemyFactory.name(), blueEnemyFactory);
+        enemyList =new ArrayList<>(Arrays.asList("BlueEnemy", "BlueEnemy"));
+    }
+    
+    public void spawn(ArrayList<String> enemyList) {
+
+        for (int i=0; i < enemyList.size(); i++){
+            MonsterFactory monsterFactory = monsterFactories.get(enemyList.get(i));
+            MonsterInterface monster = monsterFactory.create(this);
+            monsterList.add(monster);
+
+        }
+    }
 
     @Override
-    public int getEnemies() {
-        return enemies;
+    public ArrayList<MonsterInterface> getMonsters() {
+        return monsterList;
     }
 
     @Override
@@ -38,27 +66,31 @@ public class Cave extends TiledMap implements MapInterface{
         return renderer;
     }
 
-
     @Override
     public float getPlayerSpawnX() {
         return PlayerSpawnX;
     }
+
     @Override
     public float getPlayerSpawnY() {
         return PlayerSpawnY;
     }
+
     @Override
     public int getEnemyBoundsFromX() {
         return EnemyBoundsfromX;
     }
+
     @Override
     public int getEnemyBoundsToX() {
         return EnemyBoundsToX;
     }
+
     @Override
     public int getEnemyBoundsFromY() {
         return EnemyBoundsFromY;
     }
+    
     @Override
     public int getEnemyBoundsToY() {
         return EnemyBoundsToY;
