f89629356e3e9bde15a33a50648b2a78458c8ebb
HC <hlo028@uib.no>
2023-03-23 12:42:39 +0100

    La til getMonsters()
    Hvert map skal ha en liste med monster som tilhører mapet

--- src/main/java/inf112/skeleton/app/Mapfolder/MapInterface.java
+++ src/main/java/inf112/skeleton/app/Mapfolder/MapInterface.java
@@ -1,17 +1,30 @@
 package inf112.skeleton.app.Mapfolder;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+import inf112.skeleton.app.Entities.MonsterInterface;
+
 
 public interface MapInterface {
-    public int getEnemies();
+
     public TiledMap getMap();
+
     public OrthogonalTiledMapRenderer getRenderer();
+    
     public float getPlayerSpawnX();
+    
     public float getPlayerSpawnY();
+    
     public int getEnemyBoundsFromX();
+    
     public int getEnemyBoundsToX();
+    
     public int getEnemyBoundsFromY();
-    public int getEnemyBoundsToY();    
+    
+    public int getEnemyBoundsToY();
+    
+    public ArrayList<MonsterInterface> getMonsters();
 }
