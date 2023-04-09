f136caf15e08eddd262214b2c99b8df47e74b577
HC <hlo028@uib.no>
2023-04-09 21:52:19 +0200

    Implementerte tester: testEnemyIsDead()

--- src/test/java/inf112/skeleton/app/TestEnemy.java
+++ src/test/java/inf112/skeleton/app/TestEnemy.java
@@ -1,5 +1,8 @@
 package inf112.skeleton.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Assertions;
@@ -38,6 +41,32 @@ public class TestEnemy {
         blueEnemy.takeDamage(22);
         Assertions.assertEquals(18, blueEnemy.getCurrentHitpoints());
         
-    }   
+    } 
+    
+    @Test
+    void testEnemyIsDead() {
+        // Mock Enemy
+        RedEnemy redEnemy = mock(RedEnemy.class, Mockito.CALLS_REAL_METHODS);
+        BlueEnemy blueEnemy = mock(BlueEnemy.class, Mockito.CALLS_REAL_METHODS);
+        
+        // Set hitpoints
+        redEnemy.setMaxhitpoints(75);
+        redEnemy.setCurrentHitPoints(redEnemy.getMaxHitpoints());
+        blueEnemy.setMaxhitpoints(50);
+        blueEnemy.setCurrentHitPoints(blueEnemy.getMaxHitpoints());
+
+        // Test if enemy is dead
+        assertFalse(redEnemy.isDead());
+        assertFalse(blueEnemy.isDead());
+
+        // Test that enemy dies
+        redEnemy.takeDamage(100);
+        assertTrue(redEnemy.isDead());
+        assertEquals(0, redEnemy.getCurrentHitpoints());
+
+        blueEnemy.takeDamage(50);
+        assertTrue(blueEnemy.isDead());
+        assertEquals(0, blueEnemy.getCurrentHitpoints());       
+    }
 
 }
