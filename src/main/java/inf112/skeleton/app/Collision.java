76c665e52433058d6960b7791a33d1695e937a2f
Asdf1234 <bjornhagen3@live.no>
2023-05-03 18:15:58 +0200

    formatert collision klassen

--- src/main/java/inf112/skeleton/app/Collision.java
+++ src/main/java/inf112/skeleton/app/Collision.java
@@ -1,4 +1,5 @@
 package inf112.skeleton.app;
+
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
@@ -9,13 +10,12 @@ import inf112.skeleton.app.Mapfolder.House;
 import inf112.skeleton.app.Mapfolder.Level1Mini;
 import inf112.skeleton.app.Mapfolder.MapInterface;
 
-
-
 public class Collision {
 
-    public MapInterface nextMap; 
-    public MapInterface currMap;   
+    public MapInterface nextMap;
+    public MapInterface currMap;
     private TiledMap map;
+
     public TiledMap getMap() {
         return map;
     }
@@ -24,11 +24,10 @@ public class Collision {
         this.map = map;
     }
 
-
     private AbstractGameObject entity;
 
     private float tileSize;
-    private float posX,posY;
+    private float posX, posY;
 
     private boolean killedAllEnemies = true;
 
@@ -43,146 +42,153 @@ public class Collision {
         boolean collisionX = false;
         posX = entity.getPosition().x;
         posY = entity.getPosition().y;
-        
-        
+
         // when moving to the left
         if (velX < 0) {
             // top left tile
-            collisionX = isCellBlocked((int) (posX+velX / tileSize), (int) ((posY + entity.getHeight() - (entity.getHeight() / 4)) / tileSize));
+            collisionX = isCellBlocked((int) (posX + velX / tileSize),
+                    (int) ((posY + entity.getHeight() - (entity.getHeight() / 4)) / tileSize));
 
             // middle left tile
             if (!collisionX)
-                collisionX = isCellBlocked((int) ((posX+velX)/ tileSize), (int) ((posY + entity.getHeight()/2) / tileSize));
+                collisionX = isCellBlocked((int) ((posX + velX) / tileSize),
+                        (int) ((posY + entity.getHeight() / 2) / tileSize));
 
             // bottom left tile
             if (!collisionX)
-                collisionX = isCellBlocked((int) (posX+velX/ tileSize), (int) (((posY + entity.getHeight() / 4) / tileSize)));
+                collisionX = isCellBlocked((int) (posX + velX / tileSize),
+                        (int) (((posY + entity.getHeight() / 4) / tileSize)));
 
-            
         }
 
         // when moving to the right
         else if (velX > 0) {
             // top right tile
-            collisionX = isCellBlocked((int) ((posX+velX + entity.getWidth())/tileSize), (int) ((posY + entity.getHeight() - (entity.getHeight() / 4))/tileSize));
+            collisionX = isCellBlocked((int) ((posX + velX + entity.getWidth()) / tileSize),
+                    (int) ((posY + entity.getHeight() - (entity.getHeight() / 4)) / tileSize));
 
-            //middle right tile
+            // middle right tile
             if (!collisionX)
-                collisionX = isCellBlocked((int) ((posX+velX + entity.getWidth())/tileSize), (int) ((posY + entity.getHeight()/2)/tileSize));
+                collisionX = isCellBlocked((int) ((posX + velX + entity.getWidth()) / tileSize),
+                        (int) ((posY + entity.getHeight() / 2) / tileSize));
 
-            //bottom right
+            // bottom right
             if (!collisionX)
-                collisionX = isCellBlocked((int) ((posX+velX + entity.getWidth())/tileSize), (int) ((posY + entity.getHeight() / 4)/tileSize));
-            }
+                collisionX = isCellBlocked((int) ((posX + velX + entity.getWidth()) / tileSize),
+                        (int) ((posY + entity.getHeight() / 4) / tileSize));
+        }
 
+        if (collisionX)
+            return true;
+        return false;
 
-            if (collisionX) return true;
-            return false;
-        
     }
 
-    public boolean checkYDirection(float velY) { 
+    public boolean checkYDirection(float velY) {
         boolean collisionY = false;
 
         // when moving downwards
         if (velY < 0) {
             // bottom left
-            collisionY = isCellBlocked((int) ((posX + entity.getWidth() - (entity.getWidth() / 4)) / tileSize), (int) ((posY+velY)/ tileSize));
+            collisionY = isCellBlocked((int) ((posX + entity.getWidth() - (entity.getWidth() / 4)) / tileSize),
+                    (int) ((posY + velY) / tileSize));
 
             // bottom middle
             if (!collisionY)
-                collisionY = isCellBlocked((int) ((posX + (entity.getWidth()/2) ) / tileSize), (int) ((posY+velY)/ tileSize));
+                collisionY = isCellBlocked((int) ((posX + (entity.getWidth() / 2)) / tileSize),
+                        (int) ((posY + velY) / tileSize));
 
-            //bottom right
+            // bottom right
             if (!collisionY)
-                collisionY =  isCellBlocked((int) ((posX + entity.getWidth() / 4 ) / tileSize), (int) ((posY+velY)/ tileSize));
-                
+                collisionY = isCellBlocked((int) ((posX + entity.getWidth() / 4) / tileSize),
+                        (int) ((posY + velY) / tileSize));
+
         }
         // moving upwards
         else if (velY > 0) {
 
             // top left
-            collisionY = isCellBlocked((int) ((posX + entity.getWidth() - (entity.getWidth() / 4)) / tileSize), (int) ((posY+velY+ entity.getHeight()) / tileSize));
+            collisionY = isCellBlocked((int) ((posX + entity.getWidth() - (entity.getWidth() / 4)) / tileSize),
+                    (int) ((posY + velY + entity.getHeight()) / tileSize));
 
-            //top middle
+            // top middle
             if (!collisionY)
-                collisionY = isCellBlocked((int) ((posX + (entity.getWidth()/2)) / tileSize), (int) ((posY+velY+ entity.getHeight()) / tileSize));
-            //top right
+                collisionY = isCellBlocked((int) ((posX + (entity.getWidth() / 2)) / tileSize),
+                        (int) ((posY + velY + entity.getHeight()) / tileSize));
+            // top right
             if (!collisionY)
-                collisionY = isCellBlocked((int) ((posX+ entity.getWidth() / 4 ) / tileSize), (int) ((posY+velY+entity.getHeight()) / tileSize));
+                collisionY = isCellBlocked((int) ((posX + entity.getWidth() / 4) / tileSize),
+                        (int) ((posY + velY + entity.getHeight()) / tileSize));
         }
-        if (collisionY) return true;
+        if (collisionY)
+            return true;
         return false;
 
     }
-    
-   
-    public boolean isCellBlocked(int xpos, int ypos) {
+
+    private boolean isCellBlocked(int xpos, int ypos) {
 
         int size = map.getLayers().size();
 
         for (int i = 0; i < size; i++) {
             TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(i);
             TiledMapTileLayer.Cell cell = layer.getCell(xpos, ypos);
-            
 
-            if (cell != null && cell.getTile().getProperties().containsKey("blocked")) { 
+            if (cell != null && cell.getTile().getProperties().containsKey("blocked")) {
                 return true;
             }
-            
+
         }
         return false;
-    } 
-
-
-    
-
+    }
 
     public boolean isCellAPortal() {
 
-        if (entity instanceof Player){
-
+        if (entity instanceof Player) {
 
             int size = map.getLayers().size();
 
+            float entityX = (entity.getPosition().x + entity.getWidth() / 2) / tileSize;
+            float entityY = (entity.getPosition().y + entity.getHeight() / 2) / tileSize;
 
-            float entityX = (entity.getPosition().x + entity.getWidth() / 2 )/tileSize;
-            float entityY =  (entity.getPosition().y + entity.getHeight()/ 2)/tileSize;
+            for (int i = 0; i < size; i++) {
 
-            
-
-            for (int i=0; i<size; i++){
-                
                 TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(i);
 
-                TiledMapTileLayer.Cell entityCell = layer.getCell((int)entityX,(int)entityY);
-                
-                if(entityCell != null && entityCell.getTile().getProperties().containsKey("portal")) {
+                TiledMapTileLayer.Cell entityCell = layer.getCell((int) entityX, (int) entityY);
 
-                    //This is for the map cycle
-                    if (entityCell.getTile().getProperties().containsKey("house")){nextMap =  new House(); currMap.stopMusic();}
+                if (entityCell != null && entityCell.getTile().getProperties().containsKey("portal")) {
 
-                    if (entityCell.getTile().getProperties().containsKey("level 2")){nextMap = new Level1Mini(113, 75);currMap.stopMusic();}
-
-                    if (entityCell.getTile().getProperties().containsKey("grass")){nextMap = new GrassMini(119,52);currMap.stopMusic();}
-
-                    if (entityCell.getTile().getProperties().containsKey("mini level 1")){
-                        
-                        if(currMap.getMonsterList().isEmpty()){
-                            nextMap = new Level1Mini(139,70);
-                            currMap.stopMusic();
-                        } else{
-                            return false;
-                        }
-                        
-                        
+                    // This is for the map cycle
+                    if (entityCell.getTile().getProperties().containsKey("house")) {
+                        nextMap = new House();
+                        currMap.stopMusic();
                     }
 
-                    
-                    
-                return true;
+                    if (entityCell.getTile().getProperties().containsKey("level 2")) {
+                        nextMap = new Level1Mini(113, 75);
+                        currMap.stopMusic();
+                    }
+
+                    if (entityCell.getTile().getProperties().containsKey("grass")) {
+                        nextMap = new GrassMini(119, 52);
+                        currMap.stopMusic();
+                    }
+
+                    if (entityCell.getTile().getProperties().containsKey("mini level 1")) {
+
+                        if (currMap.getMonsterList().isEmpty()) {
+                            nextMap = new Level1Mini(139, 70);
+                            currMap.stopMusic();
+                        } else {
+                            return false;
+                        }
+
+                    }
+
+                    return true;
                 }
-            } 
+            }
         }
         return false;
     }
