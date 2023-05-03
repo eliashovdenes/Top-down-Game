635cc759e9e32ec8453ff392eea4396b1746c102
Elias Hovdenes <email@example.com>
2023-05-03 15:53:40 +0200

    Direction enum test

--- src/test/java/inf112/skeleton/app/Entities/EnumsTests/DirectionEnumTest.java
+++ src/test/java/inf112/skeleton/app/Entities/EnumsTests/DirectionEnumTest.java
@@ -3,9 +3,21 @@ package inf112.skeleton.app.Entities.EnumsTests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+import inf112.skeleton.app.Entities.Enums.DirectionEnum;
+
 public class DirectionEnumTest {
     @Test
-    void test(){
-        fail();
+    public void testDirectionEnum() {
+        assertEquals(DirectionEnum.WEST, DirectionEnum.valueOf("WEST"));
+        assertEquals(DirectionEnum.EAST, DirectionEnum.valueOf("EAST"));
+        assertEquals(DirectionEnum.NORTH, DirectionEnum.valueOf("NORTH"));
+        assertEquals(DirectionEnum.SOUTH, DirectionEnum.valueOf("SOUTH"));
+
+        assertEquals(DirectionEnum.WEST.toString(), "WEST");
+        assertEquals(DirectionEnum.EAST.toString(), "EAST");
+        assertEquals(DirectionEnum.NORTH.toString(), "NORTH");
+        assertEquals(DirectionEnum.SOUTH.toString(), "SOUTH");
+
+        assertEquals(DirectionEnum.values().length, 4);
     }
 }
