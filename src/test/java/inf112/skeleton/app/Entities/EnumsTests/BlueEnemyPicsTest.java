b9c8087d9c4d2978e8bfd0d0defb80b085045cd5
Elias Hovdenes <email@example.com>
2023-05-03 15:24:44 +0200

    skrevet test til en enum

--- src/test/java/inf112/skeleton/app/Entities/EnumsTests/BlueEnemyPicsTest.java
+++ src/test/java/inf112/skeleton/app/Entities/EnumsTests/BlueEnemyPicsTest.java
@@ -3,9 +3,14 @@ package inf112.skeleton.app.Entities.EnumsTests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+import inf112.skeleton.app.Entities.Enums.BlueEnemyPics;
+
 public class BlueEnemyPicsTest {
     @Test
-    void test(){
-        fail();
+    public void testBlueEnemyPics() {
+        assertEquals(BlueEnemyPics.ENEMYDOWN.source, "assets/enemyPics/enemyDown.png");
+        assertEquals(BlueEnemyPics.ENEMYUP.source, "assets/enemyPics/enemyUp.png");
+        assertEquals(BlueEnemyPics.ENEMYLEFT.source, "assets/enemyPics/enemyLeft.png");
+        assertEquals(BlueEnemyPics.ENEMYRIGHT.source, "assets/enemyPics/enemyRight.png");
     }
 }
