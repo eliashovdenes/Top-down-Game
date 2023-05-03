d189d3aaa29545a4d2ef73b6466ebfa97d43cdae
Asdf1234 <bjornhagen3@live.no>
2023-05-03 13:37:46 +0200

    implementert test for zelda class

--- src/test/java/inf112/skeleton/app/ZeldaTest.java
+++ src/test/java/inf112/skeleton/app/ZeldaTest.java
@@ -1,14 +1,37 @@
 package inf112.skeleton.app;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import inf112.skeleton.app.Controller.Controller;
+
 public class ZeldaTest {
+
+    private static Zelda game;
+    private static Controller controller;
+
+    
+    @BeforeAll
+	static void setUpBeforeAll() {
+        
+        game = new Zelda();
+        controller = new Controller();
+	}
+
     @Test
-    void testPause(){
-        fail();
+    public void testGetController() {
+        
+        assertNotNull(controller);
+        assertTrue(controller instanceof Controller);
     }
-    @Test 
-    void testResume(){
+
+    @Test
+    public void testCreate(){
+        //TODO: implement test (fikk det ikke til!)
         fail();
     }
 }
