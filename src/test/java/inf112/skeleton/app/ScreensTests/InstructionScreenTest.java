506549119aed39af896537dbf3cac7cb9287bac4
Asdf1234 <bjornhagen3@live.no>
2023-05-03 18:16:39 +0200

    endret rekkefølge på 2 linjer (setsize før rektangel blir laget

--- src/test/java/inf112/skeleton/app/ScreensTests/InstructionScreenTest.java
+++ src/test/java/inf112/skeleton/app/ScreensTests/InstructionScreenTest.java
@@ -1,11 +1,1 @@
 package inf112.skeleton.app.ScreensTests;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
-
-public class InstructionScreenTest {
-    @Test
-    void test(){
-        fail();
-    }
-}
