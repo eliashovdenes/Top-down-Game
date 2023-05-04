08c021282b967887994fed24d5c4881ee6519f6f
Bjorn <bjornhagen3@live.no>
2023-05-04 15:05:43 +0200

    mocker Southgame i alle tester

--- src/test/java/inf112/skeleton/app/ZeldaTest.java
+++ src/test/java/inf112/skeleton/app/ZeldaTest.java
@@ -2,7 +2,7 @@ package inf112.skeleton.app;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -18,7 +18,7 @@ public class ZeldaTest {
     @BeforeAll
 	static void setUpBeforeAll() {
         
-        game = new Southgame();
+        game = mock(Southgame.class);
         controller = new Controller();
 	}
 
