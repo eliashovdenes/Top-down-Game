262112e93a01ae54e3cf65f7c682bbeca16170fe
Elias Hovdenes <email@example.com>
2023-05-04 10:20:45 +0200

    Test for ID test

--- src/test/java/inf112/skeleton/app/Entities/EnumsTests/IDTest.java
+++ src/test/java/inf112/skeleton/app/Entities/EnumsTests/IDTest.java
@@ -3,9 +3,19 @@ package inf112.skeleton.app.Entities.EnumsTests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+import inf112.skeleton.app.Entities.Enums.ID;
+
 public class IDTest {
     @Test
-    void test(){
-        fail();
+    public void testID() {
+        assertEquals(ID.Player, ID.valueOf("Player"));
+        assertEquals(ID.Enemy, ID.valueOf("Enemy"));
+        assertEquals(ID.Thing, ID.valueOf("Thing"));
+
+        assertEquals(ID.Player.toString(), "Player");
+        assertEquals(ID.Enemy.toString(), "Enemy");
+        assertEquals(ID.Thing.toString(), "Thing");
+
+        assertEquals(ID.values().length, 3);
     }
 }
