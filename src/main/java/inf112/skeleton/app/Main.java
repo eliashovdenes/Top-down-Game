e1728ce79e731b5228fc335ed54943f0c8106fa5
HC <hlo028@uib.no>
2023-05-05 11:18:45 +0200

    Fjernet feltvariabler som ikke var i bruk

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
-    public static int screenWidth = 1920;
-    public static int screenHeight = 1080;
-
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
 
